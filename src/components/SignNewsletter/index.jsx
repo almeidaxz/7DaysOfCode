5fc58b8e43686d7e749844a05920ae06d077625e
almeidaxz <almeidaxz@live.com>
2022-10-13 12:07:54 -0300

    newsletter card done

--- src/components/SignNewsletter/index.jsx
+++ src/components/SignNewsletter/index.jsx
@@ -6,7 +6,7 @@ export default function Newsletter() {
         <div className="newsletter-container">
             <span className='font-size-22'>Sua casa com as</span>
             <br />
-            <strong className='highlight font-size-82'>melhores plantas</strong>
+            <strong className='highlight font-size-82 highlight-font-family'>melhores plantas</strong>
             <p className="p-box font-size-16">Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
             <div className='custom-input row align-center'>
                 <img src={Mail} className='mail-icon' alt="mail icon" />
