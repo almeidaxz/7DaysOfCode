2c22420d74e94c1a2ae13d0c7e3938b99357815f
almeidaxz <almeidaxz@live.com>
2022-10-13 12:14:09 -0300

    navbar done

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,6 @@
 import Logo from '../../assets/logo.svg';
 import '../../styles/utils.css';
+import './styles.css';
 
 const menuOptions = ['Como fazer', 'Ofertas', 'Depoimenmtos', 'Videos', 'Meu Carrinho'];
 
@@ -7,14 +8,14 @@ export default function NavBar() {
     console.log(menuOptions.at(-1));
     return (
         <header>
-            <nav className='row align-center space-btw'>
+            <nav className='row align-center space-btw font-size-16'>
                 <img src={Logo} alt="page logo" />
                 <div className='row gap-13'>
                     {menuOptions.map((item) => {
                         return (
                             <div key={item} className='row gap-13'>
                                 <div>
-                                    <a href='#'>{item}</a>
+                                    <a className='menu-item' href='#'>{item}</a>
                                 </div>
                                 {item === menuOptions.at(-1) ? '' : <div>/</div>}
                             </div>
